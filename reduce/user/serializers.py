6f1eb1fe829726c6cd10672b434977d8619ac42a
gustos <jucie15@nate.com>
2017-10-11 15:43:42 +0900

    modified user/serializer.py 유저 생성시 손님인지, 사장인지 구분하여 생성

--- reduce/user/serializers.py
+++ reduce/user/serializers.py
@@ -8,13 +8,26 @@ class OwnerSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
 
+    is_owner = serializers.BooleanField(default=False, required=False,  write_only=True)
+    owner = OwnerSerializer(default=None, required=False)
+
     class Meta:
         model = User
-        fields = ('username', 'password', 'phone_number',)
+        fields = ('username', 'password', 'phone_number', 'is_owner', 'owner')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User(**validated_data)
+        is_owner = validated_data.pop('is_owner', False)
+        owner_data = validated_data.pop('owner', None)
+
+        if is_owner == True:
+            owner = Owner.objects.create(**owner_data)
+            customer = None
+        else:
+            owner = None
+            customer = Customer.objects.create()
+
+        user = User(customer=customer, owner=owner, **validated_data)
         user.set_password(validated_data['password'])
         user.save()
         return user
