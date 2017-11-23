fe0c731963298d6333c920a4b5023156d68d7bd7
gustos <jucie15@nate.com>
2017-11-23 19:15:16 +0900

    가게별 평균 평점 필드 추가

--- reduce/api/models.py
+++ reduce/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from user.models import Customer
 from owner.models import Shop
 
@@ -8,3 +9,12 @@ class Review(models.Model):
     shop = models.ForeignKey(Shop) # 가게와 1:N 관계 설정
     content = models.TextField(max_length=128) # 리뷰의 내용
     rating = models.DecimalField(max_digits=2, decimal_places=1) # 리뷰에서 가게의 별점
+
+    def on_post_save(sender, **kwargs):
+        # 리뷰 작성시 실행
+        review = kwargs['instance']
+        if kwargs['created']:
+            #지금 생성된게 맞다면
+            review.shop.update_rating()
+
+post_save.connect(Review.on_post_save, sender=Review)
